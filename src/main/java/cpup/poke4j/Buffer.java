a38cd00a57f33ad07e395709545eab6bb447934b
CoderPuppy <cpup@cpup-laptop>
2014-05-15 10:49:13 -0400

    Add lines to Buffer

--- src/main/java/cpup/poke4j/Buffer.java
+++ src/main/java/cpup/poke4j/Buffer.java
@@ -1,14 +1,26 @@
 package cpup.poke4j;
 
+import java.util.Vector;
+
 public class Buffer {
 	protected final Poke poke;
+	protected final Vector<String> lines;
+
+	public Buffer(Poke _poke, Vector<String> _lines) {
+		poke = _poke;
+		lines = _lines;
+	}
 
 	public Buffer(Poke _poke) {
-		poke = _poke;
+		this(_poke, new Vector<String>());
 	}
 
 	// Getters and Setters
 	public Poke getPoke() {
 		return poke;
+	}
+
+	public Vector<String> getLines() {
+		return lines;
 	}
 }
