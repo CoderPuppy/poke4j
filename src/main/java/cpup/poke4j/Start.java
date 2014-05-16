9c2df49c3d62de5ebf6d9b312d14ca497d6ae2f9
CoderPuppy <cpup@cpup-laptop>
2014-05-16 11:58:04 -0400

    Testing

--- src/main/java/cpup/poke4j/Start.java
+++ src/main/java/cpup/poke4j/Start.java
@@ -2,6 +2,7 @@ package cpup.poke4j;
 
 import cpup.poke4j.gui.MainWindow;
 import cpup.poke4j.operations.InsertOperation;
+import cpup.poke4j.operations.RemoveOperation;
 import cpup.poke4j.plugin.CommandRun;
 import cpup.poke4j.plugin.files.LoadCommand;
 import cpup.poke4j.plugin.files.SaveCommand;
@@ -12,13 +13,15 @@ import java.util.Arrays;
 public class Start {
 	public static void main(String[] args) {
 		final Poke poke = new Poke();
-		final InsertOperation.Data data = new InsertOperation(0, 0, "HI!").apply(poke.getCurrentBuffer());
-		//poke.getCurrentBuffer().insertImpl(0, 0, "HI!");
-		poke.getCurrentBuffer().insertImpl(2, 0, " World");
-		System.out.println("[" + poke.getCurrentBuffer().removeImpl(2, 0, 1) + "]");
-		poke.getCurrentBuffer().insertImpl(2, 0, "\n");
-		new CommandRun(poke, LoadCommand.get(), Arrays.asList(new Object[] { "test.txt" })).invoke();
-		new CommandRun(poke, SaveCommand.get(), Arrays.asList(new Object[] { "test.txt" })).invoke();
+
+		final Buffer buffer = poke.getCurrentBuffer();
+
+		buffer.apply(new InsertOperation(-1, -1, "Hello"));
+		buffer.apply(new InsertOperation(-1, -1, "\nWorld"));
+		buffer.apply(new InsertOperation(-1, -2, "\nfoo\nbar\nbaz"));
+
+//		new CommandRun(poke, LoadCommand.get(), Arrays.asList(new Object[] { "test.txt" })).invoke();
+//		new CommandRun(poke, SaveCommand.get(), Arrays.asList(new Object[] { "test.txt" })).invoke();
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
