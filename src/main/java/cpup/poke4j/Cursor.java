40a78ad9383c2a835a1e54dd26829a97e2573bdd
CoderPuppy <cpup@cpup-laptop>
2014-05-20 21:20:09 -0400

    Add ways of setting the selection on Cursor

--- src/main/java/cpup/poke4j/Cursor.java
+++ src/main/java/cpup/poke4j/Cursor.java
@@ -7,11 +7,16 @@ public class Cursor {
 	protected int line;
 	protected Selection selection;
 
-	public Cursor(Poke _poke, Buffer _buffer, int _column, int _line) {
+	public Cursor(Poke _poke, Buffer _buffer, int _column, int _line, Selection _selection) {
 		poke = _poke;
 		buffer = _buffer;
 		column = _column;
 		line = _line;
+		selection = _selection;
+	}
+
+	public Cursor(Poke _poke, Buffer _buffer, int _column, int _line) {
+		this(_poke, _buffer, _column, _line, null);
 	}
 
 	// Getters and Setters
@@ -33,5 +38,9 @@ public class Cursor {
 
 	public Selection getSelection() {
 		return selection;
+	}
+
+	public void setSelection(Selection selection) {
+		this.selection = selection;
 	}
 }
