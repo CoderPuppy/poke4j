d80897fad6ad2e1a9ef552449f9d98522a9c9480
CoderPuppy <cpup@cpup-laptop>
2014-05-26 21:11:30 -0400

    Use InsertCommand, Add deleting and Fix the crazy characters being added

--- src/main/java/cpup/poke4j/plugin/input/BasicMode.java
+++ src/main/java/cpup/poke4j/plugin/input/BasicMode.java
@@ -4,14 +4,20 @@ import cpup.poke4j.Buffer;
 import cpup.poke4j.Cursor;
 import cpup.poke4j.Poke;
 import cpup.poke4j.plugin.CommandRun;
+import cpup.poke4j.plugin.editing.InsertCommand;
+import cpup.poke4j.plugin.editing.RemoveCommand;
 import cpup.poke4j.plugin.js.JSArray;
 import cpup.poke4j.plugin.movement.MoveLRCommand;
 import cpup.poke4j.plugin.movement.MoveUDCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.event.KeyEvent;
 import java.util.List;
 
 public class BasicMode extends Mode {
+	public static final Logger logger = LoggerFactory.getLogger(BasicMode.class);
+
 	@Override
 	public void handle(Input input) {
 		final Poke poke = input.getPoke();
@@ -20,6 +26,7 @@ public class BasicMode extends Mode {
 		final boolean alt = input.isAltDown();
 		if(input instanceof KeyInput) {
 			final KeyInput keyInput = (KeyInput) input;
+			final KeyInput.Type keyType = keyInput.getType();
 			final int code = keyInput.getKeyCode();
 
 			if(code == KeyEvent.VK_LEFT) {
@@ -31,24 +38,17 @@ public class BasicMode extends Mode {
 			} else if(code == KeyEvent.VK_UP) {
 				new CommandRun(poke, buffer, MoveUDCommand.get(), JSArray.of(-1)).invoke();
 			} else if(code == KeyEvent.VK_BACK_SPACE) {
-				// TODO: implement
-				// new CommandRun(poke, buffer, DeleteCommand.get(), JSArray.of(-1)).invoke();
+				new CommandRun(poke, buffer, RemoveCommand.get(), JSArray.of(-1)).invoke();
 			} else if(code == KeyEvent.VK_DELETE) {
-				// TODO: implement
-				// new CommandRun(poke, buffer, DeleteCommand.get(), JSArray.of(1)).invoke();
-			} else if(keyInput.getType() == KeyInput.Type.type) {
-				for(Cursor cursor : buffer.getCursors()) {
-					cursor.setSelection(null);
-					buffer.insert(cursor.getColumn(), cursor.getLine(), Character.toString(keyInput.getKeyChar()));
-					if(!ctrl) {
-						cursor.move(1);
-					}
-				}
+				new CommandRun(poke, buffer, RemoveCommand.get(), JSArray.of(1)).invoke();
+			} else if(keyType == KeyInput.Type.type && !ctrl && keyInput.getKeyCode() != 0) {
+				new CommandRun(poke, buffer, InsertCommand.get(), JSArray.of(Character.toString(keyInput.getKeyChar()))).invoke();
 			}
 		} else if(input instanceof MouseInput) {
 			final MouseInput mouseInput = (MouseInput) input;
+			final MouseInput.Type mouseType = mouseInput.getType();
 
-			if(mouseInput.getType() == MouseInput.Type.click) {
+			if(mouseType == MouseInput.Type.click) {
 				final List<Cursor> cursors = buffer.getCursors();
 				final int line = buffer.findLine(mouseInput.getLine());
 				final int column = buffer.findColumn(mouseInput.getColumn(), line);
